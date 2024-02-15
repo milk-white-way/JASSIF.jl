97886bf013fcbe0ad7babaf1683c076da936ca49
milk-white-way <tam.thien.nguyen@ndus.edu>
2024-02-15 14:42:25 -0600

    Fix variable initialization for better indexing

--- src/matfile/Init.m
+++ src/matfile/Init.m
@@ -1,68 +1,102 @@
-function [Ucont_x Ucont_y Ucat_x Ucat_y Ubcs_x Ubcs_y Pressure dx dy] = Init(M2,N2)
-% This function initialize the driven cavity problem
-L = 1;%pi
-dx = L / (M2-3);
-dy = L / (N2-3);
+function [Ucont_x, Ucont_y, Ucat_phy_x, Ucat_phy_y, Pressure_phy, Ucat_cal_x,  Ucat_cal_y, Pressure_cal, Ubcs_x, Ubcs_y, Pbcs, dx, dy] = Init(M, N, M2, N2, M3, N3)
 
-% Zero out everything
-for i=1:M2
-    for j = 1:N2
-        
-        x = (i-1.5) * dx;
-        y = (j-1.5) * dy;
-        
-        Ucat_x(i,j) = 0;
-        Ucat_y(i,j) = 0;
+L = 1; % Square domain
+dx = L / M;
+dy = L / N;
 
-        saveCoordinates('coor_ucat.csv', x, y);
+%% Allocate memory for the components
+Ucont_x = nan(M3, N2);
+Ucont_y = nan(M2, N3);
+
+Ucat_phy_x = nan(M, N);
+Ucat_phy_y = nan(M, N);
+Pressure_phy = nan(M, N);
+
+Ubcs_x = nan(M2, N2);
+Ubcs_y = nan(M2, N2);
+Pbcs = nan(M2, N2);
+
+Ucat_cal_x = nan(M2, N2);
+Ucat_cal_y = nan(M2, N2);
+Pressure_cal = nan(M2, N2);
+
+%% Init cell-centered components, ghost cells included
+for i = 1:M
+    for j = 1:N
+        
+        %x = (i-1) * dx;
+        %y = (j-1) * dy;
+        
+        Ucat_phy_x(i,j) = 0;
+        Ucat_phy_y(i,j) = 0;
+        Pressure_phy(i,j) = 0;
+
+        %saveCoordinates('coor_cell_centered.csv', x, y);
     end
 end
 
+for i=1:M2
+    for j = 1:N2
 
-% Setup contravariant component
-for i = 1:M2+1
+        %x = (i-1.5) * dx;
+        %y = (j-1.5) * dy;
+
+        if i == 1 || i == M2 || j == 1 || j == N2
+            Ubcs_x(i,j) = 0;
+            Ubcs_y(i,j) = 0;
+            Pbcs(i,j) = 0;
+
+            %saveCoordinates('coor_ghost.csv', x, y);
+        end
+
+    end
+end
+
+%% Init face-centered components
+% x-component
+for i = 1:M3
     for j = 1:N2
         
-        x = (i-2) * dx;
-        y = (j-1.5) * dy;                
+        %x = (i-2) * dx;
+        %y = (j-1.5) * dy;                
         
         Ucont_x(i,j) =  0;
 
-        saveCoordinates('coor_ucont_x.csv', x, y);
+        %saveCoordinates('coor_ucont_x.csv', x, y);
         
     end
 end
 
-
+% y-component
 for i = 1:M2
-    for j = 1:N2+1
+    for j = 1:N3
         
-        x = (i-1.5) * dx;
-        y = (j-2) * dy;        
+        %x = (i-1.5) * dx;
+        %y = (j-2) * dy;        
         
 
         Ucont_y(i,j) = 0;
 
-        saveCoordinates('coor_ucont_y.csv', x, y);
+        %saveCoordinates('coor_ucont_y.csv', x, y);
     end
 end
 
-% Set boundary condition
-Ubcs_x(1:M2,1:N2)= 0;
-Ubcs_y(1:M2,1:N2)= 0;
-
-% Set the pressure
+%% Create calculated domain
 for i=1:M2
     for j = 1:N2
-        
-        x = (i-2) * dx;
-        y = (j-2) * dy;       
-    
-        Pressure(i,j) = 0;
 
-        saveCoordinates('coor_press.csv', x, y);
+        %x = (i-1.5) * dx;
+        %y = (j-1.5) * dy;
+
+        if i == 1 || i == M2 || j == 1 || j == N2
+            Ucat_cal_x(i,j) = Ubcs_x(i,j);
+            Ucat_cal_y(i,j) = Ubcs_y(i,j);
+            Pressure_cal(i,j) = Pbcs(i,j);
+            %saveCoordinates('coor_ghost.csv', x, y);
+        else 
+            Ucat_cal_x(i,j) = Ucat_phy_x(i-1,j-1);
+            Ucat_cal_y(i,j) = Ucat_phy_y(i-1,j-1);
+            Pressure_cal(i,j) = Pressure_phy(i-1,j-1);
+        end
     end
 end
-
-
-
--- src/matfile/Main.m
+++ src/matfile/Main.m
@@ -1,53 +1,70 @@
-function [Ucat_x Ucat_y Pressure dx dy M N t] = Main()
+function [Ucat_phy_x, Ucat_phy_y, Pressure_phy, dx, dy, t] = Main()
 
-clear all
+clearvars; close all;
+
 format long;
 
-M = 3;
-N = 3;
-% Add two more ghost points
+% Variables' dimension in non-staggered grid
+M = 4;
+N = 4;
+
+% Ghost variables' dimension in non-staggered grid
 M2 = M+2;
 N2 = N+2;
 
+% Variables' dimension in staggered grid
+M3 = M+3;
+N3 = N+3;
+
 dt = 0.05;
 MAXTIME = 200;%round(1/dt);
-%Note for Taylor Problem Re=1
+
 Re = 100;
-% Remember dU for RK
+
+% Initialization process
 dU_x = 0;
 dU_y = 0;
-% Initialization process
-[Ucont_x Ucont_y Ucat_x Ucat_y Ubcs_x Ubcs_y Pressure dx dy] = Init(M2,N2);%_Taylor_Green(M2,N2);
+t = 0;
+[Ucont_x, Ucont_y, ...
+    Ucat_phy_x, Ucat_phy_y, Pressure_phy, ...
+    Ucat_cal_x, Ucat_cal_y, Pressure_cal, ...
+    Ubcs_x, Ubcs_y, Pbcs, dx, dy] = Init(M,N, M2, N2, M3, N3)
 
 % It is the time integration scheme
-for time_step = 1:MAXTIME       
-       
-    tic
-    time_step
-    t = time_step * dt    
-       
-    [U_im_x U_im_y] =  Momentum_Solver(dU_x , dU_y, Ucont_x, Ucont_y, Ucat_x, Ucat_y, Ubcs_x, Ubcs_y, Pressure, Re, dx, dy, dt,t);        
-     
-    [phi]  = Poisson_Solver(U_im_x, U_im_y, dx, dy, dt);
-    
-    [U_x_new U_y_new P_new] = Update_Solution(U_im_x, U_im_y, Pressure, phi,dx,dy,dt);
-    
-    %Update dU
-    dU_x = U_x_new - Ucont_x;
-    dU_y = U_y_new - Ucont_y;
-    
-    % Go next time step
-    [Ucat_x Ucat_y Ucont_x Ucont_y] = FormBCS(U_x_new, U_y_new, Ubcs_x, Ubcs_y, dx, dy,Re,t);
-    
-    Pressure = P_new;          
+CALCULATE = 0;
+while CALCULATE
+    for time_step = 1:MAXTIME       
         
-    % Assess the divergence of flow field
-    Div = Divergence(U_x_new, U_y_new,dx ,dy);
-    MaxDiv = norm(Div,inf)
-   
-    toc             
-    
-    norm(Vectorize(dU_x),inf)
+        tic
+        time_step
+        t = time_step * dt    
+            
+        [U_im_x U_im_y] =  Momentum_Solver(dU_x , dU_y, Ucont_x, Ucont_y, Ucat_x, Ucat_y, Ubcs_x, Ubcs_y, Pressure, Re, dx, dy, dt,t);        
+            
+        [phi]  = Poisson_Solver(U_im_x, U_im_y, dx, dy, dt);
+            
+        [U_x_new U_y_new P_new] = Update_Solution(U_im_x, U_im_y, Pressure, phi,dx,dy,dt);
+            
+        %Update dU
+        dU_x = U_x_new - Ucont_x;
+        dU_y = U_y_new - Ucont_y;
+            
+        % Go next time step
+        [Ucat_x Ucat_y Ucont_x Ucont_y] = FormBCS(U_x_new, U_y_new, Ubcs_x, Ubcs_y, dx, dy,Re,t);
+            
+        Pressure = P_new;          
+            
+        % Assess the divergence of flow field
+        Div = Divergence(U_x_new, U_y_new,dx ,dy);
+        MaxDiv = norm(Div,inf)
+        
+        toc             
+            
+        norm(Vectorize(dU_x),inf)
+    end
+    if time_step == MAXTIME
+        CALCULATE = 0;
+    end
 end
     
 
